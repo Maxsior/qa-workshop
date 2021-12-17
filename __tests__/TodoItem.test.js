3381ef1fa66d9d22fc89dd4a8d929df7cc8298c3
Maksim Kudryavtsev <m.kudryavtsev@xsolla.com>
2021-12-17 15:44:57 +0500

    Use testing-library

---
+++ __tests__/TodoItem.test.js
@@ -0,0 +1,33 @@
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+
+import TodoItem from '../src/components/TodoItem';
+
+let container = null;
+
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+test('TodoItem.todo.title', () => {
+    // Arrange
+    const todo = {
+        title: 'Test todo',
+        done: false,
+    };
+
+    // Act
+    act(() => {
+        render(<TodoItem todo={todo} />, container);
+    });
+
+    // Accept
+    expect(container.textContent).toBe(todo.title);
+});
